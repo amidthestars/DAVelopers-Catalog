e13e4ea144856e86936f47bbac1277ecfa719b3c
Claire <1867claire@gmail.com>
2024-04-28 16:56:22 -0700

    changes

--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -5,34 +5,38 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  //const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        {/* <Heading as="h1" className="hero__title">
           {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        </Heading> */}
+        <p className="hero__subtitle">
+          {
+          "Hello! Welcome to the DAV IP Catalog. Here, you’ll find modules designed by past DAV students for a variety of applications. Any module published on this website is available to you to use for your own DAV capstone or any other Verilog project. Happy coding :-)"
+          }</p>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  // const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`DAV IP Catalog}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
